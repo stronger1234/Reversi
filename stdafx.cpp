3266c08d68360d2874400a90828c311169368bfb
CZZ <8181317+stronger1234@users.noreply.github.com>
2021-10-29 23:55:13 +0800

    简化链接选项杂注。

--- stdafx.cpp
+++ stdafx.cpp
@@ -6,3 +6,9 @@
 
 // TODO: 在 STDAFX.H 中引用任何所需的附加头文件，
 //而不是在此文件中引用
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#pragma comment(linker, R"("/manifestdependency:name='Microsoft.Windows.Common-Controls' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' type='win32' version='6.0.0.0'")")
